5bfc62ff64a3285417d264c59ce54fc00afc2b14
Caique Oliveira <cadosoliveira.develcode@rd.com.br>
2023-08-08 11:55:34 -0300

    feature/semana-devops

---
+++ docker/src/server.js
@@ -0,0 +1,41 @@
+// Add this to the very top of the first file loaded in your app
+var apm = require('elastic-apm-node').start({
+  serviceName: 'lauch-store',
+
+  secretToken: 'pQa2kHWAiUGsgO4M9L',
+
+  serverUrl: 'https://f60c5a3efa52455e8f1ddd4c36a6054d.apm.sa-east-1.aws.cloud.es.io:443',
+
+  environment: 'production'
+})
+
+const express = require('express');
+const nunjucks = require('nunjucks');
+const methodOverride = require('method-override');
+const routes = require('./routes');
+const session = require('./config/session');
+
+const server = express();
+
+server.use(session);
+server.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static('public'));
+server.use(methodOverride('_method'));
+server.use(routes);
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('src/app/views', {
+  express: server,
+  autoescape: false,
+  noCache: true
+});
+
+server.listen(80, function () {
+  console.log('server is running on port 80.');
+});
